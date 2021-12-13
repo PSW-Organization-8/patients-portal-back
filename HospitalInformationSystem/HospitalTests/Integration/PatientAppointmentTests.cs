60967566cfad22a60ccbe0b44ff77d0746172c5a
RadisaMilovcevic <73460919+RadisaMilovcevic@users.noreply.github.com>
2021-12-13 20:43:42 +0100

    fix: Advanced appointment integration test fix

--- HospitalInformationSystem/HospitalTests/Integration/PatientAppointmentTests.cs
+++ HospitalInformationSystem/HospitalTests/Integration/PatientAppointmentTests.cs
@@ -8,6 +8,7 @@ using HospitalClassLib.Schedule.Repository.PatientRepository;
 using HospitalClassLib.Schedule.Service;
 using HospitalClassLib.SharedModel;
 using HospitalClassLib.SharedModel.Enums;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -37,14 +38,16 @@ namespace HospitalTests.Integration
             FillFakeDatabase();
             var appointmentController = new AppointmentController(new AppointmentService(new AppointmentRepository(context), new DoctorRepository(context)), 
                 new DoctorService(new DoctorRepository(context)), new PatientService(new PatientRepository(context)));
-            var availableAppointments = appointmentController.GetAppointmentByPriority(new DateTime(2021, 12, 15, 8, 0, 0), new DateTime(2021, 12, 16, 16, 0, 0), 3, true);
+            var availableAppointments = appointmentController.GetAppointmentByPriority(new DateTime(2022, 12, 15, 8, 0, 0), new DateTime(2022, 12, 16, 16, 0, 0), 100, true);
             List<DateTime> dateTimes = new List<DateTime>();
-            FillDateTimeFrame(new DateTime(2021, 12, 13, 8, 0, 0), new DateTime(2021, 12, 13, 15, 45, 0), dateTimes);
-            FillDateTimeFrame(new DateTime(2021, 12, 14, 8, 0, 0), new DateTime(2021, 12, 14, 15, 45, 0), dateTimes);
-            FillDateTimeFrame(new DateTime(2021, 12, 17, 8, 0, 0), new DateTime(2021, 12, 17, 15, 45, 0), dateTimes);
-            FillDateTimeFrame(new DateTime(2021, 12, 18, 8, 0, 0), new DateTime(2021, 12, 18, 15, 45, 0), dateTimes);
+            FillDateTimeFrame(new DateTime(2022, 12, 13, 8, 0, 0), new DateTime(2022, 12, 13, 15, 45, 0), dateTimes);
+            FillDateTimeFrame(new DateTime(2022, 12, 14, 8, 0, 0), new DateTime(2022, 12, 14, 15, 45, 0), dateTimes);
+            FillDateTimeFrame(new DateTime(2022, 12, 17, 8, 0, 0), new DateTime(2022, 12, 17, 15, 45, 0), dateTimes);
+            FillDateTimeFrame(new DateTime(2022, 12, 18, 8, 0, 0), new DateTime(2022, 12, 18, 15, 45, 0), dateTimes);
 
-           // Assert.Equal(dateTimes, availableAppointments);
+            var availableAppointmentsCheck = availableAppointments as ObjectResult;
+
+            Assert.Equal(dateTimes, availableAppointmentsCheck.Value);
         }
 
         [Fact]
@@ -53,29 +56,30 @@ namespace HospitalTests.Integration
             FillFakeDatabase();
             var appointmentController = new AppointmentController(new AppointmentService(new AppointmentRepository(context), new DoctorRepository(context)), 
                 new DoctorService(new DoctorRepository(context)), new PatientService(new PatientRepository(context)));
-            var availableAppointments = appointmentController.GetAppointmentByPriority(new DateTime(2021, 12, 15, 8, 0, 0), new DateTime(2021, 12, 16, 16, 0, 0), 3, false);
+            var availableAppointments = appointmentController.GetAppointmentByPriority(new DateTime(2021, 12, 15, 8, 0, 0), new DateTime(2021, 12, 16, 16, 0, 0), 100, false);
             List<DateTime> dateTimes = new List<DateTime>();
             FillDateTimeFrame(new DateTime(2021, 12, 15, 8, 0, 0), new DateTime(2021, 12, 15, 15, 45, 0), dateTimes);
             FillDateTimeFrame(new DateTime(2021, 12, 16, 8, 0, 0), new DateTime(2021, 12, 16, 15, 45, 0), dateTimes);
 
-           // Assert.Equal(dateTimes, availableAppointments);
+            var availableAppointmentsCheck = availableAppointments as ObjectResult;
+
+            Assert.Equal(dateTimes, availableAppointmentsCheck.Value);
         }
 
         private void FillDateTimeFrame(DateTime firstDate, DateTime lastDate, List<DateTime> dateTimes)
         {
             for (DateTime dateTime = firstDate; dateTime <= lastDate; dateTime = dateTime.AddMinutes(15))
-                if (dateTime != new DateTime(2021, 12, 12, 12, 0, 0))
                     dateTimes.Add(dateTime);
         }
 
         private void FillFakeDatabase()
         {
-            context.Doctors.Add(new Doctor { Id = 3, DoctorSpecialization = Specialization.FamilyPhysician });
-            context.Patients.Add(new Patient { Id = 5 });
-            for (DateTime dateTime = new(2021, 12, 15, 8, 0, 0); dateTime <= new DateTime(2021, 12, 15, 15, 45, 0); dateTime = dateTime.AddMinutes(15))
-                context.Appointments.Add(new Appointment { StartTime = dateTime, DoctorId = 3, PatientId = 5});
-            for (DateTime dateTime = new(2021, 12, 16, 8, 0, 0); dateTime <= new DateTime(2021, 12, 16, 15, 45, 0); dateTime = dateTime.AddMinutes(15))
-                context.Appointments.Add(new Appointment { StartTime = dateTime, DoctorId = 3, PatientId = 5 });
+            context.Doctors.Add(new Doctor { Id = 100, DoctorSpecialization = Specialization.FamilyPhysician });
+            context.Patients.Add(new Patient { Id = 50 });
+            for (DateTime dateTime = new(2022, 12, 15, 8, 0, 0); dateTime <= new DateTime(2022, 12, 15, 15, 45, 0); dateTime = dateTime.AddMinutes(15))
+                context.Appointments.Add(new Appointment { StartTime = dateTime, DoctorId = 100, PatientId = 50});
+            for (DateTime dateTime = new(2022, 12, 16, 8, 0, 0); dateTime <= new DateTime(2022, 12, 16, 15, 45, 0); dateTime = dateTime.AddMinutes(15))
+                context.Appointments.Add(new Appointment { StartTime = dateTime, DoctorId = 100, PatientId = 50});
             context.SaveChanges();
         }
     }
