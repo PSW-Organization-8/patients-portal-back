d5eb5f5fd0223d2212cf8c9617c6f01330d292d5
RadisaMilovcevic <73460919+RadisaMilovcevic@users.noreply.github.com>
2022-01-10 14:10:03 +0100

    fix: Patient validation fix

--- HospitalInformationSystem/HospitalAPI/Validators/PatientValidator.cs
+++ HospitalInformationSystem/HospitalAPI/Validators/PatientValidator.cs
@@ -11,6 +11,7 @@ namespace HospitalAPI.Validators
             RuleFor(patient => patient.Name).Matches("^[a-zA-Z]+$").WithMessage("Name can contains only letters");
             RuleFor(patient => patient.LastName).Matches("^[a-zA-Z]+$").WithMessage("Last name can contains only letters");
             RuleFor(patient => patient.Jmbg).Matches("[0-9]{13}").WithMessage("Jmbg must contain 13 numerics and cannot have any other characters");
+            RuleFor(patient => patient.Contact.Email).EmailAddress();
         }
     }
 }
